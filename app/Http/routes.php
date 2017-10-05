7dc75cadaa403d3fc1d01be85cb56c91db717bc8
introqt <introtheintro@gmail.com>
2017-10-05 17:45:49 +0300

    продолжили проект с сайта ларавель добавляем роуты

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,17 +1,93 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+use App\Task;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+  /**
+   * Вывести панель с задачами
+   */
+  Route::get('/', function () 
+  {
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+    //dump('tasks');
+    return view('tasks', [
+        'tasks'=>$tasks,
+    ]);
+  });
 
+  /**
+   * Добавить новую задачу
+   */
+  Route::post('/task', function (Request $request) 
+  {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+  ]);
+    if ($validator->fails()) 
+    {
+      return redirect('/')
+        ->withInput()
+        ->withErrors($validator);
+    }
+    //dump('no errors');
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+  });
+
+  /**
+   * Удалить задачу
+   */
+  Route::delete('/task/{task}', function (Task $task) 
+  {
+    $task->delete();
+    return redirect('/');
+  });
+  
+  /**
+   * Обновить задачу
+   */
+    Route::post('/task/eddit', function (Request $request) 
+  {
+    $eddit = update::make($request->all(), [
+        'name' => 'required|max:255',
+  ]);
+
+    if ($eddit->fails()) 
+    {
+      return redirect('/')
+        ->withInput()
+        ->withErrors($eddit);
+    }
+    dump('no errors');
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+  });
+    
+  /**
+   * Сохранить задачу
+   */
+  Route::save('/task', function (Request $request) 
+  {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+  ]);
+
+    if ($validator->fails()) 
+    {
+      return redirect('/')
+        ->withInput()
+        ->withErrors($validator);
+    }
+    //dump('no errors');
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+  });
